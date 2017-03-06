5be92e0faf572c6e329e5ad4de5ee9f8f983d29d
Anton Te <te.anton@gmail.com>
2017-03-05 18:28:47 -0800

    Remove coddle config

--- coddle.cfg/cfg.cpp
+++
@@ -1,9 +0,0 @@
-#include <coddle/coddle.hpp>
-#include <coddle/config.hpp>
-
-int main(int argc, char **argv)
-{
-  Config config(argc, argv);
-  config.pkgs.push_back("sdl2");
-  return coddle(&config);
-}
